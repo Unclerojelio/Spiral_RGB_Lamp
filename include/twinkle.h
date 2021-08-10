dd9bba009f43fcbd61c6c40f9a6955b7ce74d17b
Roger Banks <roger_banks@mac.com>
2021-08-10 16:36:53 -0500

    Add comments and documentation. Improve readability.

--- include/twinkle.h
+++ include/twinkle.h
@@ -38,21 +38,34 @@ void DrawTwinkle()
     }      
 }
 
+/******************************************************************************
+ * Function: DrawTwinkleTails
+ * Date 09 Aug 2021
+ * Created by: Roger Banks
+ * Description: Creates a random string of pixels with a random color from a
+ *              table and moves that string of pixels along the strip until
+ *              it fades out.
+ *****************************************************************************/
 void DrawTwinkleTails()
 {
-    const int fadeAmt = 64;
-    const int tail_size = 3;
+    const int fadeAmt = 64;            // amount to fade pixels by every pass
+    const int tailSize = 3;            // length of the string of pixels
     EVERY_N_MILLISECONDS(250)
     {
-        int random_pos = random(tail_size, NUM_LEDS-1);
+        // choose a random position within the strip
+        int random_pos = random(tailSize, NUM_LEDS-1);
+        // choose a random color from the table
         CRGB random_color = TwinkleColors[random(0, ARRAYSIZE(TwinkleColors))];
-        //FastLED.leds()[random(NUM_LEDS)] = TwinkleColors[random(0, ARRAYSIZE(TwinkleColors))];
-        for(int j = random_pos; j > random_pos - tail_size; j--) {
+        // assign the chosen color to the chosen pixel and the pixels following
+        // it according the tail_size
+        for(int j = random_pos; j > random_pos - tailSize; j--) {
             FastLED.leds()[j] = random_color;
         }
+        // animate the pixels to move forward along the strip
         for(int i = NUM_LEDS-1; i > 1; i--) {
             FastLED.leds()[i] = FastLED.leds()[i-1];
         }
+        // fade all the pixels by the fadeAmt
         fadeToBlackBy(FastLED.leds(), NUM_LEDS, fadeAmt);
     }      
 }
